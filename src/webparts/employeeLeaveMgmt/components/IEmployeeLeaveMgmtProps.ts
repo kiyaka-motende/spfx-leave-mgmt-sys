3cc263077df40abfaa07787cccf0f810b92f68c0
kiyaka-motende <hkingjr.hm@gmail.com>
2023-01-09 13:58:54 +0300

    Added spfx react webpart folder structures and initial files. Also added pnpjsConfig file for handling authentication.

---
+++ src/webparts/employeeLeaveMgmt/components/IEmployeeLeaveMgmtProps.ts
@@ -0,0 +1,7 @@
+export interface IEmployeeLeaveMgmtProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/employeeLeaveMgmt/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IEmployeeLeaveMgmtWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'EmployeeLeaveMgmtWebPartStrings' {
+  const strings: IEmployeeLeaveMgmtWebPartStrings;
+  export = strings;
+}
